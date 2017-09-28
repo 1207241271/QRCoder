fd8c9dd122fdbeacbeb7442393bc63f2a2e48a3e
1207241271 <1207241271@qq.com>
2017-09-28 18:38:39 +0800

    文件名修改

--- app/src/main/java/com/xunce/qrcoder/MainActivity.java
+++ app/src/main/java/com/xunce/qrcoder/MainActivity.java
@@ -4,9 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -17,6 +19,7 @@ import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +35,8 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements QRCodeView.Delega
     private ZXingView   mQRCodeView;
     private HashMap<String ,String> IMEIMap;
     private TextView txt_SN;
+    private TextView txt_File;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements QRCodeView.Delega
         });
 
         txt_SN = (TextView) findViewById(R.id.txt_SN);
+        txt_File = (TextView) findViewById(R.id.txt_file);
 
         IMEIMap = new HashMap<>();
 
@@ -83,7 +90,27 @@ public class MainActivity extends AppCompatActivity implements QRCodeView.Delega
                     }
                 }).create().show();
                 txt_SN.setText(IMEIMap.get(IMEI));
+            }else {
+                vibrate();
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("IMEI号错误"+IMEI);
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
             }
+        }else {
+            vibrate();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("没有IMEI");
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
         }
         mQRCodeView.startCamera();
         mQRCodeView.startSpot();
@@ -146,6 +173,7 @@ public class MainActivity extends AppCompatActivity implements QRCodeView.Delega
 
                 }
             }
+
             inputReader.close();//关闭InputStreamReader
             bufferedReader.close();//关闭BufferedReader
         }catch (Exception e){
@@ -153,33 +181,70 @@ public class MainActivity extends AppCompatActivity implements QRCodeView.Delega
         }
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (resultCode == Activity.RESULT_OK&&requestCode==0){
-//            Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
-//            String string =uri.getPath().toString();
-//            File file = new File(string);
-//
-//                IMEIMap.clear();
-//                try {
-//                    InputStreamReader read = new InputStreamReader(new FileInputStream(file), "UTF-8");// 考虑到编码格式
-//                    BufferedReader bufferedReader = new BufferedReader(read);
-//                    String lineTxt = null;
-//                    while ((lineTxt = bufferedReader.readLine()) != null) {//按行读取
-//                        // System.out.println(“lineTxt=” + lineTxt);
-//                        if (!"".equals(lineTxt)) {
-//                            String [] strArray = lineTxt.split(" ");//对行的内容进行分析处理后再放入map里。
-//                            IMEIMap.put(strArray[0],strArray[1]);
-//                        }
-//                    }
-//                    read.close();//关闭InputStreamReader
-//                    bufferedReader.close();//关闭BufferedReader
-//                }catch (Exception e){
-//                    e.printStackTrace();
-//                }
-//
-//        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK&&requestCode==0){
+            Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
+            String string = null;
+            try {
+                string = getPath(this, uri);
+                String[] showFileName = string.split("/");
+                txt_File.setText(showFileName[showFileName.length-1]);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            if (TextUtils.isEmpty(string)){
+                return;
+            }
+            File file = new File(string);
+                IMEIMap.clear();
+                try {
+                    InputStreamReader read = new InputStreamReader(new FileInputStream(file), "UTF-8");// 考虑到编码格式
+                    BufferedReader bufferedReader = new BufferedReader(read);
+                    String lineTxt = null;
+                    while ((lineTxt = bufferedReader.readLine()) != null) {//按行读取
+                        // System.out.println(“lineTxt=” + lineTxt);
+                        if (!"".equals(lineTxt)) {
+                            String [] strArray = lineTxt.split("\\t");//对行的内容进行分析处理后再放入map里。
+                            IMEIMap.put(strArray[0],strArray[1]);
+                        }
+                    }
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("读取文件成功");
+                    builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+                    read.close();//关闭InputStreamReader
+                    bufferedReader.close();//关闭BufferedReader
+                    mQRCodeView.startCamera();
+                    mQRCodeView.startSpot();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+        }
+    }
+    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {"_data"};
+            Cursor cursor = null;
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it  Or Log it.
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
 
     private void createDir(){
         File filePath = this.getFilesDir();
